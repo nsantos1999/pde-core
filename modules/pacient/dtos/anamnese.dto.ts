bac7c692d10301f1d7ed71fc1f7443ea42c7dc83
Natã Souza Santos <natasouza.santos50@gmail.com>
2024-04-24 13:35:43 -0300

    🐛 dto problems

--- modules/pacient/dtos/anamnese.dto.ts
+++ modules/pacient/dtos/anamnese.dto.ts
@@ -1,6 +1,6 @@
-import { TimeAvailability } from '../../../global/@types/schedule.types';
-import { Gender } from '../../user/constants/enums/gender.enum';
-import { PacientMetadata } from '../@types/pacient.type';
+import { TimeAvailability } from "../../../global/@types/schedule.types";
+import { Gender } from "../../user/constants/enums/gender.enum";
+import { PacientMetadata } from "../@types/pacient.type";
 
 export interface AnamneseDtoInterface {
   name: string;
@@ -9,7 +9,7 @@ export interface AnamneseDtoInterface {
   confirmPassword: string;
   phone: string;
   age: string;
-  gender: Gender;
+  gender?: Gender;
   time_availability: TimeAvailability;
 
   metadata: PacientMetadata;
