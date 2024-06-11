6d7b227b3f7b3432b027ac24e1d28cde77f20660
Natã Souza Santos <natasouza.santos50@gmail.com>
2024-06-11 08:07:06 -0300

    ✨ add files on content dto

--- modules/content/dtos/create-content.dto.ts
+++ modules/content/dtos/create-content.dto.ts
@@ -1,11 +1,15 @@
 export interface ContentVideoDtoInterface {
+  id?: number;
   order: number;
   title: string;
+  banner?: File;
+  video?: File;
 }
 
 export interface CreateContentDtoInterface {
   title: string;
   author: string;
   themeId: number;
+  banner?: File;
   videos: ContentVideoDtoInterface[];
 }
