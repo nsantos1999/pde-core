40c49602621e8ed18fbcdb1073b8242e6c25b5b5
Natã Souza Santos <natasouza.santos50@gmail.com>
2025-03-10 19:43:54 -0300

    feat: add description property to UpdateDoctorConfigsDtoInterface

--- modules/doctor/dtos/update-doctor-configs.dto.ts
+++ modules/doctor/dtos/update-doctor-configs.dto.ts
@@ -1,7 +1,8 @@
-import { TimeAvailability } from '../../../global/@types/schedule.types';
+import { TimeAvailability } from "../../../global/@types/schedule.types";
 
 export interface UpdateDoctorConfigsDtoInterface {
   time_availability: TimeAvailability;
   approach: string[];
   crp_number: string;
+  descrition: string;
 }
