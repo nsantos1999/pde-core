afaa056fde9130afb05d12abaf8a20fd2995097d
Natã Souza Santos <natasouza.santos50@gmail.com>
2025-03-13 17:00:02 -0300

    fix: update import path for TimeAvailability in UpdateDoctorConfigsDtoInterface

--- modules/doctor/dtos/update-doctor-configs.dto.ts
+++ modules/doctor/dtos/update-doctor-configs.dto.ts
@@ -1,4 +1,4 @@
-import { TimeAvailability } from "../../../global/@types/schedule.types";
+import { TimeAvailability } from '@/libs/pde-core/global/@types/schedule.types';
 
 export interface UpdateDoctorConfigsDtoInterface {
   time_availability: TimeAvailability;
