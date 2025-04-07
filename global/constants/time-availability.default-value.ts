5463c7402947a95f97c8f0a82c8307c304928ecb
Natã Souza Santos <natasouza.santos50@gmail.com>
2025-04-07 18:30:01 -0300

    feat: add default time availability values for weekdays

---
+++ global/constants/time-availability.default-value.ts
@@ -0,0 +1,39 @@
+import { TimeAvailability, Weekday } from "../@types/schedule.types";
+
+export const timeAvailabilityDefaultValues: TimeAvailability = {
+  [Weekday.SUNDAY]: {
+    is_active: false,
+    label: "Domingo",
+    timeframes: [],
+  },
+  [Weekday.MONDAY]: {
+    is_active: false,
+    label: "Segunda",
+    timeframes: [],
+  },
+  [Weekday.TUESDAY]: {
+    is_active: false,
+    label: "Terça",
+    timeframes: [],
+  },
+  [Weekday.WEDNESDAY]: {
+    is_active: false,
+    label: "Quarta",
+    timeframes: [],
+  },
+  [Weekday.THURSDAY]: {
+    is_active: false,
+    label: "Quinta",
+    timeframes: [],
+  },
+  [Weekday.FRIDAY]: {
+    is_active: false,
+    label: "Sexta",
+    timeframes: [],
+  },
+  [Weekday.SATURDAY]: {
+    is_active: false,
+    label: "Sabado",
+    timeframes: [],
+  },
+};
