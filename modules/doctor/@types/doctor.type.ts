ed20abce237bf344cf29aa392fd94d120782034f
Natã Souza Santos <natasouza.santos50@gmail.com>
2025-04-08 19:17:59 -0300

    feat: update therapyTypes property in DoctorType interface to use DoctorTherapyType

--- modules/doctor/@types/doctor.type.ts
+++ modules/doctor/@types/doctor.type.ts
@@ -3,7 +3,7 @@ import { UserType } from '../../user/@types/user.type';
 import { TimeAvailability } from '../../../global/@types/schedule.types';
 import { MultiSelectOption } from '../../../global/@types/multiselect-option.types';
 import { DoctorCharacteristicType } from './doctor-characteristic.type';
-import { TherapyTypesEnum } from '../../appointments/constants/enums/therapy-types.enum';
+import { DoctorTherapyType } from './doctor-therapy-type.type';
 
 export interface DoctorType {
   doctor_id: number;
@@ -17,7 +17,7 @@ export interface DoctorType {
   days: DayType[];
   time_availability: TimeAvailability;
   characteristics: DoctorCharacteristicType[];
-  therapyTypes: TherapyTypesEnum[];
+  therapyTypes: DoctorTherapyType[];
   is_active: boolean;
   created_at: Date;
   updated_at: Date;
