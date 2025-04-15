c2154829a9d8c8e910bc226fe386b67c6fba5da0
Natã Souza Santos <natasouza.santos50@gmail.com>
2025-04-14 21:02:22 -0300

    feat: add frequency and validateCode properties to GetMatchDoctorsDto

--- modules/doctor/dtos/get-match-doctors.dto.ts
+++ modules/doctor/dtos/get-match-doctors.dto.ts
@@ -1,3 +1,4 @@
+import { RecurrenceFrequencyCode } from '../../../global/@types/schedule.types';
 import { TherapyTypesEnum } from '../../appointments/constants/enums/therapy-types.enum';
 import { ApproachEnum } from '../constants/enums/approach.enum';
 import { DoctorCharacteristicsEnum } from '../constants/enums/doctor-characteristics.enum';
@@ -6,4 +7,6 @@ export class GetMatchDoctorsDto {
   therapyType: TherapyTypesEnum;
   doctorCharacteristics: DoctorCharacteristicsEnum[];
   approachPreference: ApproachEnum;
+  frequency: RecurrenceFrequencyCode;
+  validateCode: string;
 }
