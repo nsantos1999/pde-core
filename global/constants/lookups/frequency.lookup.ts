ec4b7e65ccdd0fb6cd97285f19d6116789a46fa4
Natã Souza Santos <natasouza.santos50@gmail.com>
2025-09-13 13:24:00 -0300

    feat: add priceBrl property to FrequencyLookupInterface and its instances

--- global/constants/lookups/frequency.lookup.ts
+++ global/constants/lookups/frequency.lookup.ts
@@ -4,19 +4,23 @@ export type FrequencyLookupInterface<T extends string | number | symbol = Freque
   [key in T]: {
     name: string;
     planName?: string;
+    priceBrl: number;
   };
 };
 
 export const FrequencyLookup: FrequencyLookupInterface = {
   [FrequencyCode.LOSE]: {
     name: 'Avulso',
+    priceBrl: 0,
   },
   [FrequencyCode.WEEKLY]: {
     name: 'Semanal',
     planName: 'Plano Pro',
+    priceBrl: 280,
   },
   [FrequencyCode.BIWEEKLY]: {
     name: 'Quinzenal',
     planName: 'Plano Start',
+    priceBrl: 140,
   },
 };
